59ef34a49030f841ba160f009d00d7c3174ad1ce
namkang321 <namkang_chutisilp@hotmail.com>
2020-03-06 23:03:55 +0700

    add editing pdf feature!

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var express = require('express');
 var multer = require('multer');
-
+var mime = require('mime');
+var crypto = require('crypto');
+const HummusRecipe = require('hummus-recipe');
 var app = express();
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        crypto.pseudoRandomBytes(16, function(err, raw) {
+            // console.log(raw.toString('hex'));
+            // cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+            cb(null, 'input' + '.' + mime.getExtension(file.mimetype));
+        });
+    }
+});
 app.use(
     multer({
         dest: __dirname + '/uploads/',
-        rename: function(fieldname, filename) {
-            return Date.now();
-        },
         limits: {
-            fileSize: 5000000 //5 MB
+            fileSize: 100000000 //100 MB
         },
         onFileSizeLimit: function(file) {
             console.log('Failed: ' + file.originalname + ' is limited');
             fs.unlink(file.path);
-        }
+        },
+        storage: storage
     }).single('file')
 );
 
@@ -23,8 +35,20 @@ app.get('/', function(req, res) {
 });
 
 app.post('/upload', function(req, res) {
-    console.log(req.file);
-    res.send(req.file);
+    const pdfDoc = new HummusRecipe('new', 'outputs/output.pdf');
+    // const pdfDoc = new HummusRecipe('uploads/input.pdf', 'outputs/output.pdf');
+    const coverPDF = 'outputs/cover.pdf';
+    const bodyPDF = 'outputs/body.pdf';
+    pdfDoc
+        .appendPage(coverPDF, [1, 2])
+        .appendPage(bodyPDF)
+        .appendPage(coverPDF, 3)
+        .endPDF();
+    res.redirect('/download');
+});
+
+app.get('/download', function(req, res) {
+    res.download('outputs/output.pdf');
 });
 
 app.listen(5555, function() {
